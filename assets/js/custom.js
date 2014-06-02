170fa60ff99ed4223088e7a117c8ede84fe2dece
francoiscabrol <francois.cabrol@live.fr>
2014-06-01 23:02:37 -0400

    Modified contacts bar

---
+++ assets/js/custom.js
@@ -0,0 +1,15 @@
+$(window).on('load', function() {
+	$(".nav-network .btn").hover(
+	  function () {
+	    $(this).addClass("btn-social");
+	    $(this).removeClass("btn-social-icon");
+	  },
+	  function () {
+	  	parentThis = this
+	  	setTimeout(function(){
+	  		$(parentThis).addClass("btn-social-icon");
+	    	$(parentThis).removeClass("btn-social");
+	    },100);
+	  }
+	);
+});
