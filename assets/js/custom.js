0a127eb58ea94bbf578dad6150e00704bbabbf8e
francoiscabrol <francois.cabrol@live.fr>
2014-07-04 20:14:02 -0400

    Modified header links

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,15 +1,20 @@
 $(window).on('load', function() {
 	$(".nav-network .btn-hover").hover(
 	  function () {
-	    $(this).addClass("btn-social");
-	    $(this).removeClass("btn-social-icon");
+	  	$(this).addClass("btn-social");
+	  	$(this).removeClass("btn-social-icon");
 	  },
 	  function () {
-	  	parentThis = this
+	 	parentThis = this
 	  	setTimeout(function(){
-	  		$(parentThis).addClass("btn-social-icon");
+	  	$(parentThis).addClass("btn-social-icon");
 	    	$(parentThis).removeClass("btn-social");
 	    },100);
+	  }
+	);
+	$("a[data-toggle='tooltip']").hover(
+	  function() {
+		$(this).tooltip("show");
 	  }
 	);
 });
